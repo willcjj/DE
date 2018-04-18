24f622b4a6002772b386f95b14c626c63edc11a4
willcjj <chenjjwill@gmail.com>
2018-04-17 20:13:44 -0400

    Add files via upload

--- error.php
+++ error.php
@@ -1,215 +1,271 @@
 <?php  
  
- $error = '';  
- if(isset($_POST["submit"]))  
- {  
+session_start();
+
+$error = array();  
+if(isset($_POST["submit"]))  
+{  
       if(empty($_POST["day1a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day1a</label>";  
+          array_push($error,"Enter Day1a");
+        //    $error = "<label class='text-danger'>Enter Day1a</label>";  
       }  
-      else if(empty($_POST["day1b"]))  
+      if(empty($_POST["day1b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day1b</label>";  
+        array_push($error,"Enter Day1b");
+        //    $error = "<label class='text-danger'>Enter Day1b</label>";  
       }  
-      else if(empty($_POST["day1c"]))  
+      if(empty($_POST["day1c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day1c</label>";  
+        array_push($error,"Enter Day1c");
+        //    $error = "<label class='text-danger'>Enter Day1c</label>";  
       }
-      else if(empty($_POST["day2a"]))  
+      if(empty($_POST["day2a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day2a</label>";  
+        array_push($error,"Enter Day2a");
+        //    $error = "<label class='text-danger'>Enter Day2a</label>";  
       }  
-      else if(empty($_POST["day2b"]))  
+      if(empty($_POST["day2b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day2b</label>";  
+        array_push($error,"Enter Day2b");
+        //    $error = "<label class='text-danger'>Enter Day2b</label>";  
       }  
-      else if(empty($_POST["day2c"]))  
+      if(empty($_POST["day2c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day2c</label>";  
+        array_push($error,"Enter Day2c");
+        //    $error = "<label class='text-danger'>Enter Day2c</label>";  
       }
-      else if(empty($_POST["day3a"]))  
+      if(empty($_POST["day3a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day3a</label>";  
+        array_push($error,"Enter Day3a");
+        //    $error = "<label class='text-danger'>Enter Day3a</label>";  
       }  
-      else if(empty($_POST["day3b"]))  
+      if(empty($_POST["day3b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day3b</label>";  
+        array_push($error,"Enter Day3b");
+        //    $error = "<label class='text-danger'>Enter Day3b</label>";  
       }  
-      else if(empty($_POST["day3c"]))  
+      if(empty($_POST["day3c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day3c</label>";  
+        array_push($error,"Enter Day3c");
+        //    $error = "<label class='text-danger'>Enter Day3c</label>";  
       }
-      else if(empty($_POST["day4a"]))  
+      if(empty($_POST["day4a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day4a</label>";  
+        array_push($error,"Enter Day4a");
+        //    $error = "<label class='text-danger'>Enter Day4a</label>";  
       }  
-      else if(empty($_POST["day4b"]))  
+      if(empty($_POST["day4b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day4b</label>";  
+        array_push($error,"Enter Day4b");
+        //    $error = "<label class='text-danger'>Enter Day4b</label>";  
       }  
-      else if(empty($_POST["day4c"]))  
+      if(empty($_POST["day4c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day4c</label>";  
+        array_push($error,"Enter Day4c");
+        //    $error = "<label class='text-danger'>Enter Day4c</label>";  
       }
-      else if(empty($_POST["day5a"]))  
+      if(empty($_POST["day5a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day5a</label>";  
+        array_push($error,"Enter Day5a");
+        //    $error = "<label class='text-danger'>Enter Day5a</label>";  
       }  
-      else if(empty($_POST["day5b"]))  
+      if(empty($_POST["day5b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day5b</label>";  
+        array_push($error,"Enter Day5b");
+        //    $error = "<label class='text-danger'>Enter Day5b</label>";  
       }  
-      else if(empty($_POST["day5c"]))  
+      if(empty($_POST["day5c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day5c</label>";  
+        array_push($error,"Enter Day5c");
+        //    $error = "<label class='text-danger'>Enter Day5c</label>";  
       }
-      else if(empty($_POST["day6a"]))  
+      if(empty($_POST["day6a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day6a</label>";  
+        array_push($error,"Enter Day6a");
+        //    $error = "<label class='text-danger'>Enter Day6a</label>";  
       }  
-      else if(empty($_POST["day6b"]))  
+      if(empty($_POST["day6b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day6b</label>";  
+        array_push($error,"Enter Day6b");
+        //    $error = "<label class='text-danger'>Enter Day6b</label>";  
       }  
-      else if(empty($_POST["day6c"]))  
+      if(empty($_POST["day6c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day6c</label>";  
+        array_push($error,"Enter Day6c");
+        //    $error = "<label class='text-danger'>Enter Day6c</label>";  
       }
-      else if(empty($_POST["day7a"]))  
+      if(empty($_POST["day7a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day7a</label>";  
+        array_push($error,"Enter Day7a");
+        //    $error = "<label class='text-danger'>Enter Day7a</label>";  
       }  
-      else if(empty($_POST["day7b"]))  
+      if(empty($_POST["day7b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day7b</label>";  
+        array_push($error,"Enter Day7b");
+        //    $error = "<label class='text-danger'>Enter Day7b</label>";  
       }  
-      else if(empty($_POST["day7c"]))  
+      if(empty($_POST["day7c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day7c</label>";  
+        array_push($error,"Enter Day7c");
+        //    $error = "<label class='text-danger'>Enter Day7c</label>";  
       }
-      else if(empty($_POST["day8a"]))  
+      if(empty($_POST["day8a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day8a</label>";  
+        array_push($error,"Enter Day8a");
+        //    $error = "<label class='text-danger'>Enter Day8a</label>";  
       }  
-      else if(empty($_POST["day8b"]))  
+      if(empty($_POST["day8b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day8b</label>";  
+        array_push($error,"Enter Day8b");
+        //    $error = "<label class='text-danger'>Enter Day8b</label>";  
       }  
-      else if(empty($_POST["day8c"]))  
+      if(empty($_POST["day8c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day8c</label>";  
+        array_push($error,"Enter Day8c");
+        //    $error = "<label class='text-danger'>Enter Day8c</label>";  
       }
-      else if(empty($_POST["day9a"]))  
+      if(empty($_POST["day9a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day9a</label>";  
+        array_push($error,"Enter Day9a");
+        //    $error = "<label class='text-danger'>Enter Day9a</label>";  
       }  
-      else if(empty($_POST["day9b"]))  
+      if(empty($_POST["day9b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day9b</label>";  
+        array_push($error,"Enter Day9b");
+        //    $error = "<label class='text-danger'>Enter Day9b</label>";  
       }  
-      else if(empty($_POST["day9c"]))  
+      if(empty($_POST["day9c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day9c</label>";  
+        array_push($error,"Enter Day9c");
+        //    $error = "<label class='text-danger'>Enter Day9c</label>";  
       }
-      else if(empty($_POST["day10a"]))  
+      if(empty($_POST["day10a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day10a</label>";  
+        array_push($error,"Enter Day10a");
+        //    $error = "<label class='text-danger'>Enter Day10a</label>";  
       }  
-      else if(empty($_POST["day10b"]))  
+      if(empty($_POST["day10b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day10b</label>";  
+        array_push($error,"Enter Day10b");
+        //    $error = "<label class='text-danger'>Enter Day10b</label>";  
       }  
-      else if(empty($_POST["day10c"]))  
+      if(empty($_POST["day10c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day10c</label>";  
+        array_push($error,"Enter Day10c");
+        //    $error = "<label class='text-danger'>Enter Day10c</label>";  
       }
-      else if(empty($_POST["day11a"]))  
+      if(empty($_POST["day11a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day11a</label>";  
+        array_push($error,"Enter Day11a");
+        //    $error = "<label class='text-danger'>Enter Day11a</label>";  
       }  
-      else if(empty($_POST["day11b"]))  
+      if(empty($_POST["day11b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day11b</label>";  
+        array_push($error,"Enter Day11b");
+        //    $error = "<label class='text-danger'>Enter Day11b</label>";  
       }  
-      else if(empty($_POST["day11c"]))  
+      if(empty($_POST["day11c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day11c</label>";  
+        array_push($error,"Enter Day11c");
+        //    $error = "<label class='text-danger'>Enter Day11c</label>";  
       }
-      else if(empty($_POST["day12a"]))  
+      if(empty($_POST["day12a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day12a</label>";  
+
+        array_push($error,"Enter Day12a");
+        //    $error = "<label class='text-danger'>Enter Day12a</label>";  
       }  
-      else if(empty($_POST["day12b"]))  
+      if(empty($_POST["day12b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day12b</label>";  
+        array_push($error,"Enter Day12b");
+        //    $error = "<label class='text-danger'>Enter Day12b</label>";  
       }  
-      else if(empty($_POST["day12c"]))  
+      if(empty($_POST["day12c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day12c</label>";  
+        array_push($error,"Enter Day12c");
+        //    $error = "<label class='text-danger'>Enter Day12c</label>";  
       }
-      else if(empty($_POST["day13a"]))  
+      if(empty($_POST["day13a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day13a</label>";  
+        array_push($error,"Enter Day13a");
+        //    $error = "<label class='text-danger'>Enter Day13a</label>";  
       }  
-      else if(empty($_POST["day13b"]))  
+      if(empty($_POST["day13b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day13b</label>";  
+        array_push($error,"Enter Day13b");
+        //    $error = "<label class='text-danger'>Enter Day13b</label>";  
       }  
-      else if(empty($_POST["day13c"]))  
+      if(empty($_POST["day13c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day13c</label>";  
+        array_push($error,"Enter Day13c");
+        //    $error = "<label class='text-danger'>Enter Day13c</label>";  
       }
-      else if(empty($_POST["day14a"]))  
+      if(empty($_POST["day14a"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day14a</label>";  
+        array_push($error,"Enter Day14a");
+        //    $error = "<label class='text-danger'>Enter Day14a</label>";  
       }  
-      else if(empty($_POST["day14b"]))  
+      if(empty($_POST["day14b"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day14b</label>";  
+        array_push($error,"Enter Day14b");
+        //    $error = "<label class='text-danger'>Enter Day14b</label>";  
       }  
-      else if(empty($_POST["day14c"]))  
+      if(empty($_POST["day14c"]))  
       {  
-           $error = "<label class='text-danger'>Enter Day14c</label>";  
+        array_push($error,"Enter Day14c");
+        //    $error = "<label class='text-danger'>Enter Day14c</label>";  
       }
-      else if(empty($_POST["q1"]))  
+      if(empty($_POST["q1"]))  
       {  
-           $error = "<label class='text-danger'>Enter q1</label>";  
+        array_push($error,"Enter q1");
+        //    $error = "<label class='text-danger'>Enter q1</label>";  
       }  
-      else if(empty($_POST["q2"]))  
+      if(empty($_POST["q2"]))  
       {  
-           $error = "<label class='text-danger'>Enter q2</label>";  
+        array_push($error,"Enter q2");
+        //    $error = "<label class='text-danger'>Enter q2</label>";  
       }  
-      else if(empty($_POST["q3"]))  
+      if(empty($_POST["q3"]))  
       {  
-           $error = "<label class='text-danger'>Enter q3</label>";  
+        array_push($error,"Enter q3");
+        //    $error = "<label class='text-danger'>Enter q3</label>";  
       }
-      else if(empty($_POST["q4"]))  
+      if(empty($_POST["q4"]))  
       {  
-           $error = "<label class='text-danger'>Enter q4</label>";  
+
+        array_push($error,"Enter q4");
+        //    $error = "<label class='text-danger'>Enter q4</label>";  
       }  
-      else if(empty($_POST["q5"]))  
+      if(empty($_POST["q5"]))  
       {  
-           $error = "<label class='text-danger'>Enter q5</label>";  
+        array_push($error,"Enter q5");
+        //    $error = "<label class='text-danger'>Enter q5</label>";  
       }  
-      else if(empty($_POST["q6"]))  
+      if(empty($_POST["q6"]))  
       {  
-           $error = "<label class='text-danger'>Enter q6</label>";  
+        array_push($error,"Enter q6");
+        //    $error = "<label class='text-danger'>Enter q6</label>";  
       }
-      else if(empty($_POST["q7"]))  
+      if(empty($_POST["q7"]))  
       {  
-           $error = "<label class='text-danger'>Enter q7</label>";  
+        array_push($error,"Enter q7");
+        //    $error = "<label class='text-danger'>Enter q7</label>";  
       }  
-      else if(empty($_POST["q8"]))  
+      if(empty($_POST["q8"]))  
       {  
-           $error = "<label class='text-danger'>Enter q8</label>";  
+        array_push($error,"Enter q8");
+        //    $error = "<label class='text-danger'>Enter q8</label>";  
       }  
-      else if(empty($_POST["q9"]))  
+      if(empty($_POST["q9"]))  
       {  
-           $error = "<label class='text-danger'>Enter q9</label>";  
+        array_push($error,"Enter q9");
+        //    $error = "<label class='text-danger'>Enter q9</label>";  
       }
-      else if(empty($_POST["q10"]))  
+      if(empty($_POST["q10"]))  
       {  
-           $error = "<label class='text-danger'>Enter q10</label>";  
+        array_push($error,"Enter q10");
+        //    $error = "<label class='text-danger'>Enter q10</label>";  
       }
     
 }
